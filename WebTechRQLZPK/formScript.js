9ba52f4b429555a983c1cc16ec8d43b0c3c336bf
blasko-balazs-levente <114399916+blasko-balazs-levente@users.noreply.github.com>
2024-05-06 15:30:10 +0200

    Update formScript.js

--- WebTechRQLZPK/formScript.js
+++ WebTechRQLZPK/formScript.js
@@ -1,26 +1,21 @@
 $(document).ready(function() {
-
-    var submittedData = {};
-
-    $('#submit').click(function() {
-        submittedData.name = $('#fName').val()+(' ')+$('#lName').val();
-        var name = submittedData.name;
-        submittedData.email = $('#email').val();
-        var email = submittedData.email;
-        submittedData.date = $('#date').val();
-        var date = submittedData.date;
-        if(name && email && date) {
-            alert('Név: ' + name + '\nE-mail: ' + email + '\nSzületési év: ' + date);
-        }else {
-            alert('Kérem töltsön ki minden egyes mezőt!');
-        }
-    });
     $('input').blur(function() {
-        if($(this).val().trim() === '') {
-            $(this).css('background-color', 'red');
-        }
-    });
-    $('input').focus(function() {
-        $(this).css('background-color', 'lightblue');
-    });
+    if($(this).val().trim() === '') {
+        $(this).css('background-color', 'red');
+    }
 });
+$('input').focus(function() {
+    $(this).css('background-color', 'lightblue');
+});
+function submitForm() {
+    var form = document.getElementById("myForm");
+    var formData = new FormData(form);
+
+    var result = "Check your data:\n";
+
+    for (const entry of formData.entries()) {
+        result += `${entry[0]}: ${entry[1]}\n`;
+    }
+
+    alert(result);
+}});
