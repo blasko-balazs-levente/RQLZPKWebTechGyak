ce7505fc8bc6400f119d012abf2761a9bb9ef951
blasko-balazs-levente <114399916+blasko-balazs-levente@users.noreply.github.com>
2024-05-13 02:25:59 +0200

    Update formScript.js

--- WebTechRQLZPK/formScript.js
+++ WebTechRQLZPK/formScript.js
@@ -6,16 +6,4 @@ $(document).ready(function() {
 });
 $('input').focus(function() {
     $(this).css('background-color', 'lightblue');
-});
-function submitForm() {
-    var form = document.getElementById("myForm");
-    var formData = new FormData(form);
-
-    var result = "Check your data:\n";
-
-    for (const entry of formData.entries()) {
-        result += `${entry[0]}: ${entry[1]}\n`;
-    }
-
-    alert(result);
-}});
+})});
